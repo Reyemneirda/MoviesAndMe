197aa1598296f581726292722d859f35ac04dfa6
Adrien Meyer <reyemneirda@gmail.com>
2019-02-24 13:59:49 +0200

    Set movie list

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Search from "./Components/Search";
+import { StyleSheet, View, TextInput, Button } from "react-native";
 
 export default class App extends React.Component {
   render() {
--- Components/Search.js
+++ Components/Search.js
@@ -1,18 +1,37 @@
 // Components/Search.js
-import React from "react";
-import { StyleSheet, View, TextInput, Button } from "react-native";
+import FilmItem from "./FilmItem";
 
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList
+} from "react-native";
+import films from "../Helpers/filmsData";
 class Search extends React.Component {
   render() {
     return (
-      <View style={{ marginTop: 50 }}>
+      <View style={styles.main_container}>
         <TextInput style={styles.textinput} placeholder="Titre du film" />
         <Button title="Rechercher" onPress={() => {}} />
+        {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
+        <FlatList
+          data={films}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => <FilmItem film={item} />}
+        />
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
   textinput: {
     marginLeft: 5,
     marginRight: 5,
